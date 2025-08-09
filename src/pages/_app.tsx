ed70ec9e37bce3f45e228b6fbf0bd2ce5c07e1cc
krishramoju2 <futureai.incorporated@gmail.com>
2025-08-09 09:01:30 +0530

    Create _app.tsx

---
+++ src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import type { AppProps } from 'next/app'
+import { AppProvider } from '../contexts/AppContext'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <AppProvider>
+      <Component {...pageProps} />
+    </AppProvider>
+  )
+}
